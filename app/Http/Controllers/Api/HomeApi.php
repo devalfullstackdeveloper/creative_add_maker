e2a4458a6edef0aeb21a3139dfa11200f925f41e
jinalpatelPHPDev <jinalss@itidoltechnologies.com>
2023-04-26 15:08:29 +0530

    code

--- app/Http/Controllers/Api/HomeApi.php
+++ app/Http/Controllers/Api/HomeApi.php
@@ -1177,7 +1177,7 @@ class HomeApi extends Controller
                 foreach ($video as $v) 
                 {
                     $data[] = array(
-                        "postId" => $v->festival->festivals->title."".$v->id,
+                        "postId" => $v->festival->title."".$v->id,
                         "id" => $v->festival_id,
                         "type" => "festival",
                         "language" => $v->language->title,
